fcb06d885cfae02ae37c743746242eb72a870317
jahnvi15 <91893464+jahnvi15@users.noreply.github.com>
2022-09-10 15:48:31 +0530

    Fixed_cards

--- zomatocards/src/components/FoodData.js
+++ zomatocards/src/components/FoodData.js
@@ -1,30 +1,30 @@
 const Fooddata = [
     {
         id: 1,
-        rname: "Massala Theoryy",
-        imgdata: "https://b.zmtcdn.com/data/pictures/9/18857339/8f53919f1175c08cf0f0371b73704f9b_o2_featured_v2.jpg?output-format=webp",
-        address: "North Indian, Biryani, Mughlai",
-        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
-        somedata: " 1175 + order placed from here recently",
-        price: "₹350 for one",
-        rating: "3.8",
-        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp"
+        rname: "Auditorium",
+        imgdata: "https://drive.google.com/uc?export=view&id=10aZQDxsJ7YhlJBVzCEE1xCSQlGsOLl5W",
+        address: "Main Gate, IGDTUW",
+        delimg: "",
+        somedata: "Can house upto 1000 people",
+        price: "",
+        rating: "",
+        arrimg: ""
     },
     {
-        id: 2,
-        rname: "Jugaadi Adda",
-        imgdata: "https://b.zmtcdn.com/data/pictures/chains/5/19295245/089cbcf1d3307542c72f77272556b28b_o2_featured_v2.jpg?output-format=webp",
-        address: "Street Food",
-        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
-        somedata: " 2525 + order placed from here recently",
-        price: "₹25 for one",
-        rating: "3.9",
-        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp"
+        id: 1,
+        rname: "Auditorium",
+        imgdata: "https://drive.google.com/uc?export=view&id=10aZQDxsJ7YhlJBVzCEE1xCSQlGsOLl5W",
+        address: "Main Gate, IGDTUW",
+        delimg: "",
+        somedata: "Can house upto 1000 people",
+        price: "",
+        rating: "",
+        arrimg: ""
     },
     {
         id: 3,
         rname: "La Milano Pizzeria",
-        imgdata: "https://b.zmtcdn.com/data/pictures/chains/1/19708611/10f90d4a69678d98662514d173b29665_o2_featured_v2.jpg",
+        imgdata: "https://drive.google.com/uc?export=view&id=1ahuIWjko0GDE2QoVL5DoILfUhdxA-etI",
         address: "Pizza, Fast Food, Pasta",
         delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
         somedata: " 650 + order placed from here recently",
