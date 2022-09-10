65219a59425b16385b1f4a5188a9bb050db19540
Himani <h.agarwal46@outlook.com>
2022-09-10 11:54:11 +0530

    zomato logo

--- zomatocards/src/components/Search.js
+++ zomatocards/src/components/Search.js
@@ -1,14 +1,21 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import "./style.css"
 import Fooddata from './FoodData'
 
 
 const Search = () => {
 
-  const[fdata, setFdata]= useState(Fooddata);
-  console.log(fdata);
+  const [fdata, setFdata] = useState(Fooddata);
+  //console.log(fdata);
+  const zomatologo = "https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png"
+
   return (
-    <div>Search</div>
+    <>
+      <div className="container d-flex justify-content-between align-items-center">
+        <img src={zomatologo} style={{ width: "8rem", position: "relative", left: "2%", cursor: "pointer" }} alt="" />
+        <h2 style={{ color: "#1b1464", cursor: "pointer" }} className="mt-3">Search Filter App</h2>
+      </div>
+    </>
   )
 }
 
